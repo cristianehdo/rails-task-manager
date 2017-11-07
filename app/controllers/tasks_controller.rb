90628d2129925d6399d607d94e55fbcf953bde21
Cristiane Silva <cristiane.hdo@gmail.com>
2017-11-07 12:28:45 +0000

    listing all tasks and creating a new task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+    redirect_to tasks_path
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# CRUD with 7 actions
+  # root to: "pages#index"
+
+  get "tasks", to: "tasks#index"
+
+  get "tasks/new", to: "tasks#new", as: :new
+  post "tasks", to: "tasks#create"
 end
